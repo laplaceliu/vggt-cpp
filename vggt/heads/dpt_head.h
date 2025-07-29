ba0f88be92f1d5a845b77a7fd0d2f9e5014f3678
laplaceliu <laplaceliu@outlook.com>
2025-07-29 10:04:10 +0800

    update dpt_head

--- vggt/heads/dpt_head.h
+++ vggt/heads/dpt_head.h
@@ -1,17 +1,34 @@
 #pragma once
 
 #include <torch/torch.h>
-#include "../utils/stack_sequential.h"
+#include <vector>
+#include <utility>
 
 namespace vggt {
 namespace heads {
 
-// Function declarations
-utils::StackSequential _make_fusion_block(int64_t features, int64_t size = -1, bool has_residual = true, int64_t groups = 1);
-
 torch::nn::Module _make_scratch(const std::vector<int64_t>& in_shape, int64_t out_shape, int64_t groups = 1, bool expand = false);
+torch::Tensor custom_interpolate(
+    torch::Tensor x,
+    std::pair<int64_t, int64_t> size = std::pair<int64_t, int64_t>(),
+    float scale_factor = -1.0f,
+    torch::nn::functional::InterpolateFuncOptions::mode_t mode = torch::kBilinear,
+    bool align_corners = true);
 
-torch::Tensor custom_interpolate(const torch::Tensor& input, const std::pair<int64_t, int64_t>& size, const torch::nn::functional::InterpolateFuncOptions::mode_t& mode = torch::kBilinear, bool align_corners = true);
+class ResidualConvUnitImpl : public torch::nn::Module {
+public:
+    ResidualConvUnitImpl(int64_t features, torch::nn::AnyModule activation, bool bn, int64_t groups = 1);
+    torch::Tensor forward(torch::Tensor x);
+
+private:
+    torch::nn::BatchNorm2d bn{nullptr};
+    int64_t groups;
+    torch::nn::Conv2d conv1{nullptr}, conv2{nullptr};
+    torch::nn::AnyModule norm1, norm2;
+    torch::nn::AnyModule activation;
+    torch::nn::Functional skip_add{nullptr};
+};
+TORCH_MODULE(ResidualConvUnit);
 
 } // namespace heads
 } // namespace vggt
